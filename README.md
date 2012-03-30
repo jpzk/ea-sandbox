0585106e0bbe43dc9f13ebb446e7f9d59ba241f1
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2012-03-30 10:17:07 +0200

    added better headings for subsections; better filenames

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ This repository is a public repository for some of my evolutionary-algorithms I 
 
 ## Basic information
 
-(1+1) means (mu+lambda) which is common in literature. mu is the amount of individuals to select and lambda is the amount of individuals to generate by mutation. There are two types of selection the plus- and comma-selection. The plus-selection considers old population and mutated population when selecting the fittest for the next generation. The comma-selection only takes account of the mutated population. 
+(1+1) means (mu+lambda), which is common in literature. mu is the amount of individuals to select and lambda is the amount of individuals to generate by mutation. There are two types of selection the plus- and comma-selection. The plus-selection considers old population and mutated population when selecting the fittest for the next generation. The comma-selection only takes account of the mutated population. 
 
-## Bitflip (1+1)-EA
+## bitflip.py: bitflip (1+1)-EA
 
-The goal of the Bitflip (1+1)-EA (bitflip-ea.py) is to generate a sequence of bits where every bit is 1. Therefore the fitness function counts the ones in a sequence of bits. The mutation of this evolutionary algorithm influences each bit. With a certain probability, which is equal for each bit, a bit flips, e.g. the state 0 changes to 1. The selection of the Bitflip (1+1)-EA is defined by just selecting between the old sequence of bits or the mutated sequence of bits.
+The goal of the Bitflip (1+1)-EA is to generate a sequence of bits where every bit is 1. Therefore the fitness function counts the ones in a sequence of bits. The mutation of this evolutionary algorithm influences each bit. With a certain probability, which is equal for each bit, a bit flips, e.g. the state 0 changes to 1. The selection of the Bitflip (1+1)-EA is defined by just selecting between the old sequence of bits or the mutated sequence of bits.
 
-## A (mu+lambda)-EA with Rechenberg
+## mlrechenberg.py: an EA with Rechenberg's rule
 
-The (mu+lambda)-EA in mulambdarechenberg.py is an evolutionary algorithm to minimize sum(map(lambda x : pow(x,2), x)), which is the sphere function for the dimensionality of x. Rechenberg's rule of chance of success increments or decrements mutation step size depending on the chance of success, which in fact is the relative amount of good solutions in a given period of generations. A good solution is compared with an reference fitness e.g. the best of the last period. If the relative amount of good solutions in a period is greater than 1/5, then increment step size. If it is lower than 1/5 decrement, and otherwise keep the last step size. The outcome of this approach is finding an optimum solution in much less time.   
+The (mu+lambda)-EA in mlrechenberg.py is an evolutionary algorithm to minimize sum(map(lambda x : pow(x,2), x)), which is the sphere function for the dimensionality of x. Rechenberg's rule of chance of success increments or decrements mutation step size depending on the chance of success, which in fact is the relative amount of good solutions in a given period of generations. A good solution is compared with an reference fitness e.g. the best of the last period. If the relative amount of good solutions in a period is greater than 1/5, then increment step size. If it is lower than 1/5 decrement, and otherwise keep the last step size. The outcome of this approach is finding an optimum solution in much less time.   
 
 ## To be continued
 
---
+++ mlrechenberg.py
@@ -0,0 +1,127 @@
+#!/bin/env python
+# encoding: utf-8
+
+''' 
+This file is part of evolutionary-algorithms-sandbox.
+
+evolutionary-algorithms-sandbox is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+evolutionary-algorithms-sandbox is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+evolutionary-algorithms-sandbox.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from random import * 
+from itertools import *
+
+'''
+mu+lambda EA with rechenberg-sigma gauss mutation,
+
+def run(dimensions, size, m, l, alpha, sigma):
+    dimensions: R^dimensions
+    size: values of [-size, size] possible
+    m: mu, size of population, amount of parents
+    l: amount of generated children
+    alpha: factor of rechenberg
+    sigma: start sigma for rechenberg
+
+Jendrik Poloczek <jendrik.poloczek@uni-oldenburg.de>
+'''
+
+# return fitness, 0 is best.
+def fitness(x):
+    return sum(map(lambda x : pow(x,2), x)) 
+
+# return combined child of parents x,y
+def combine(pair): 
+    return map(lambda i,j : (i+j)/2.0, pair[0], pair[1]) 
+
+# create a list in d length and in interval [-size, size]
+def create(d, size): 
+    return map(
+        lambda x : ((x * random()) - 0.5) * size * 2,
+        [1] * d)
+
+# mutate child with gauss devriation 
+def mutate(child, sigma):
+    return map(lambda value : value + gauss(0, sigma), child)
+
+# return all combination of parents
+def shuffled_parent_pairs(parents): 
+    return sorted(
+        [x for x in combinations(parents,2)],
+        key=lambda i : random())
+
+# return sorted (best, smallest, first) list of parents
+def sortedbest(children):
+    return sorted(children, key=lambda child : fitness(child))
+
+# return new sigma (rechenberg)
+def rechenberg(old_sigma, success_probability, alpha):
+    if success_probability > 1.0/5.0: return old_sigma / alpha
+    if success_probability < 1.0/5.0: return old_sigma * alpha
+    return sigma
+
+# return success_probabilty (rechenberg)
+def success_probability(children, success_fitness):
+    return len(filter(
+        lambda child: fitness(child) <= success_fitness, 
+        children)) / len(children)
+
+# return l children combined with combine function, 
+# and mutated with mutate function.
+def generate_children(parents, l, sigma):
+    return map(mutate, 
+        map(combine, shuffled_parent_pairs(parents))[:l],
+        [sigma] * l)
+
+# main evolution 
+def _run((population, generation, m, l, lastfitness,\
+    alpha, sigma)):
+
+    # generate l-children union with parents, 
+    # take the m best.
+    children = generate_children(population, l, sigma)
+    next_population = sortedbest(population + children)[:m]
+   
+    fitness_of_best = fitness(next_population[0])
+    fitness_of_worst = fitness(next_population[m-1])
+
+    # only for visual output purpose.
+    print "generation " + str(generation) +\
+        " worst fitness " + str(fitness_of_worst) +\
+        " best fitness " + str(fitness_of_best)
+
+    # update sigma according to rechenberg.
+    new_sigma = rechenberg(
+        sigma, 
+        success_probability(children, lastfitness),
+        alpha)
+
+    if(-1*pow(10,-10) < fitness_of_best < pow(10,-10)):
+        print next_population[0]
+        return True
+    else:
+        return (next_population, generation + 1, m,\
+        l, fitness_of_best, alpha, new_sigma)
+
+def run(dimensions,size,m,l,alpha,sigma):
+    # create random population d-dimensional 
+    # in interval [-s, s]
+    result = _run(
+        ([create(dimensions,size) for x in range(0,m)],
+        0, m, l, 0, alpha, sigma))
+
+    while result != True:
+        result = _run(result)
+
+    return result
+
+print run(3,10,15,100,0.5, 1)
