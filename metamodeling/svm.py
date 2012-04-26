a955f843b9913a23b23db9b33bdd0e8024a086b5
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2012-04-26 11:43:49 +0200

    embedded ea-code into test environment class

--- metamodeling/svm.py
+++ metamodeling/svm.py
@@ -23,7 +23,7 @@ from sklearn import svm
 
 '''
 mu+lambda EA with rechenberg-sigma gauss mutation for minimizing
-sum(map(lambda x : pow(x,2), x)) with tangent restriction
+sum(map(lambda x : pow(x,2), x)) with tangent constraint 
 sum(x) - 2.0 >= 0 for each valid solution. 
 
 generate_valid_population is a python generator, which generates
@@ -44,98 +44,111 @@ def run(dimensions, size, m, l, alpha, sigma):
 Jendrik Poloczek <jendrik.poloczek@uni-oldenburg.de>
 '''
 
-# return true if solution is valid, otherwise false.
-def is_valid(x):
-    return sum(x) - 2.0 >= 0
+class TestEnvironment:
 
-# return fitness, 0 is best.
-def fitness(x):
-    return sum(map(lambda x : pow(x,2), x)) 
+    _count_is_valid = 0
+    _count_fitness = 0
 
-# return combined child of parents x,y
-def combine(pair): 
-    return map(lambda i,j : (i+j)/2.0, pair[0], pair[1]) 
+    def print_statistics(self):
+        print("constraint function calls: " + str(self._count_is_valid))
+        print("fitness function calls: " + str(self._count_fitness))
 
-# mutate child with gauss devriation 
-def mutate(child, sigma):
-    return map(lambda value : value + gauss(0, sigma), child)
+    # return true if solution is valid, otherwise false.
+    def is_valid(self, x):
+        self._count_is_valid = self._count_is_valid + 1
+        return sum(x) - 2.0 >= 0
 
-# return sorted (best, smallest, first) list of parents
-def sortedbest(children):
-    return sorted(children, key=lambda child : fitness(child))
+    # return fitness, 0 is best.
+    def fitness(self, x):
+        self._count_fitness = self._count_fitness + 1
+        return sum(map(lambda x : pow(x,2), x)) 
 
-# return new sigma (rechenberg)
-def rechenberg(old_sigma, success_probability, alpha):
-    if success_probability > 1.0/5.0: return old_sigma / alpha
-    if success_probability < 1.0/5.0: return old_sigma * alpha
-    return sigma
+    # return combined child of parents x,y
+    def combine(self, pair): 
+        return map(lambda i,j : (i+j)/2.0, pair[0], pair[1]) 
 
-# return success_probabilty (rechenberg)
-def success_probability(children, success_fitness):
-    return len(filter(
-        lambda child: fitness(child) <= success_fitness, 
-        children)) / len(children)
+    # mutate child with gauss devriation 
+    def mutate(self, child, sigma):
+        return map(lambda value : value + gauss(0, sigma), child)
 
-# python generator for inifinte list of valid parent population
-def generate_valid_population(d, size):
-    while(True):
-        parent = map(lambda x : ((x * random()) - 0.5) * size *2,
-            [1] * d)
-        if(is_valid(parent)):
-            yield(parent)            
+    # return sorted (best, smallest, first) list of parents
+    def sortedbest(self, children):
+        return sorted(children, key=lambda child : self.fitness(child))
 
-# python generator for infinite list of valid children with 
-# mutated and recombined with given parents.
-def generate_valid_children(parents, sigma):
-    while(True):
-        child = mutate(combine(sample(parents,2)),
-            sigma)
-        if(is_valid(child)):
-            yield child
+    # return new sigma (rechenberg)
+    def rechenberg(self, old_sigma, success_probability, alpha):
+        if success_probability > 1.0/5.0: return old_sigma / alpha
+        if success_probability < 1.0/5.0: return old_sigma * alpha
+        return sigma
 
-# main evolution 
-def _run((population, generation, m, l, lastfitness,\
-    alpha, sigma)):
+    # return success_probabilty (rechenberg)
+    def success_probability(self, children, success_fitness):
+        return len(filter(
+            lambda child: self.fitness(child) <= success_fitness, 
+            children)) / len(children)
 
-    # generate l-children union with parents, 
-    # take the m best.
-    childgen = generate_valid_children(population, sigma)
-    children = [childgen.next() for child in range(0,l)]
+    # python generator for inifinte list of valid parent population
+    def generate_valid_population(self, d, size):
+        while(True):
+            parent = map(lambda x : ((x * random()) - 0.5) * size *2,
+                [1] * d)
+            if(self.is_valid(parent)):
+                yield(parent)            
 
-    next_population = sortedbest(population + children)[:m]
+    # python generator for infinite list of valid children with 
+    # mutated and recombined with given parents.
+    def generate_valid_children(self, parents, sigma):
+        while(True):
+            child = self.mutate(self.combine(sample(parents,2)),
+                sigma)
+            if(self.is_valid(child)):
+                yield child
+
+    # main evolution 
+    def _run(self, (population, generation, m, l, lastfitness,\
+        alpha, sigma)):
+
+        # generate l-children union with parents, 
+        # take the m best.
+        childgen = self.generate_valid_children(population, sigma)
+        children = [childgen.next() for child in range(0,l)]
+
+        next_population = self.sortedbest(population + children)[:m]
    
-    fitness_of_best = fitness(next_population[0])
-    fitness_of_worst = fitness(next_population[m-1])
+        fitness_of_best = self.fitness(next_population[0])
+        fitness_of_worst = self.fitness(next_population[m-1])
 
-    # only for visual output purpose.
-    print "generation " + str(generation) +\
+        # only for visual output purpose.
+        print "generation " + str(generation) +\
         " biggest fitness " + str(fitness_of_worst) +\
         " smallest fitness " + str(fitness_of_best)
 
-    # update sigma according to rechenberg.
-    new_sigma = rechenberg(
-        sigma, 
-        success_probability(children, lastfitness),
-        alpha)
+        # update sigma according to rechenberg.
+        new_sigma = self.rechenberg(
+            sigma, 
+            self.success_probability(children, lastfitness),
+            alpha)
 
-    if(2-1*pow(10,-3) < fitness_of_best < 2+1*pow(10,-3)):
-        print next_population[0]
-        return True
-    else:
-        return (next_population, generation + 1, m,\
-        l, fitness_of_best, alpha, new_sigma)
+        if(2-1*pow(10,-3) < fitness_of_best < 2+1*pow(10,-3)):
+            print next_population[0]
+            return True
+        else:
+            return (next_population, generation + 1, m,\
+            l, fitness_of_best, alpha, new_sigma)
 
-def run(dimensions,size,m,l,alpha,sigma):
-    # create random population d-dimensional 
-    # in interval [-s, s] but valid!
-    genpop = generate_valid_population(dimensions, size)   
-    result = _run(
-        ([genpop.next() for x in range(0,m)],
-        0, m, l, 0, alpha, sigma))
+    def run(self, dimensions, size, m, l, alpha, sigma):
+        # create random population d-dimensional 
+        # in interval [-s, s] but valid!
+        genpop = self.generate_valid_population(dimensions, size)   
+        result = self._run(
+            ([genpop.next() for x in range(0,m)],
+            0, m, l, 0, alpha, sigma))
 
-    while result != True:
-        result = _run(result)
+        while result != True:
+            result = self._run(result)
 
-    return result
+        return result
 
-print run(2,10,15,100,0.5, 1)
+env = TestEnvironment()
+env.run(2, 10, 15, 100, 0.5, 1)
+env.print_statistics()
