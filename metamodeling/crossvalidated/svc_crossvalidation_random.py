2b65765630a0d0631637fda96c080c43d61d77b3
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2012-05-02 14:50:40 +0200

    added first classes for crossvalidation approach

---
+++ metamodeling/crossvalidated/svc_crossvalidation_random.py
@@ -0,0 +1,30 @@
+''' 
+This file is part of evolutionary-algorithms-sandbox.
+
+evolutionary-algorithms-sandbox is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+evolutionary-algorithms-sandbox is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+evolutionary-algorithms-sandbox.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from svc_parameterized_meta_model import SVCParameterizedMetaModel
+from svc_crossvalidation_strategy import SVCCrossvalidationStrategy
+
+class SVCCrossvalidationRandom(SVCCrossvalidationStrategy):
+    """ A strategy for crossvalidation """
+
+    def __init__(self, fold):
+        print("fold")
+
+    def tune_svc(self, feasible, infeasible):
+        """ This method returns a pair (C, gamma) with classifcation rate
+            is maximized. """
+        print("tune")            
